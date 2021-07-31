67127678291e10e6e15bb091e096f2461b5bb8cd
Hpollard003 <hpollard003@gmail.com>
2021-07-30 20:55:47 -0400

    Done.

--- lib/keys.rb
+++ lib/keys.rb
@@ -10,4 +10,13 @@ require 'pry'
 
 def get_the_keys(groceries)
   #code your solution here!
+  groceries.keys
+end
+
+def get_the_min(groceries)
+  groceries.values.flatten.min
+end
+
+def get_the_values(groceries)
+  groceries.values.flatten
 end
